fd34cd87604c4eb7eef5db20c206077a829fd337
Kostadin Krstev <krstev.kostadin@gmail.com>
2017-10-02 21:03:01 +0200

    modified the calculations completely

--- src/mk/ukim/finki/a0/Zadaca3.java
+++ src/mk/ukim/finki/a0/Zadaca3.java
@@ -2,7 +2,9 @@ package mk.ukim.finki.a0;
 
 public class Zadaca3 {
     public static boolean isPrime(int num) {
-        if (num == 2)
+        if (num == 1)
+            return false;
+        else if (num == 2)
             return true;
         for (int j = 2; j <= (num / 2); ++j) {
             if (num % j == 0)
@@ -12,11 +14,14 @@ public class Zadaca3 {
     }
 
     public static void main(String[] args) {
-        int limit = 100;
-        int sum = 1;
-        for (int i = 2; i <= limit; ++i) {
-            if (isPrime(i))
+        int sum = 0, counter = 0;
+        for (int i = 1; ; ++i) {
+            if (isPrime(i)) {
+                ++counter;
                 sum += i;
+            }
+            if (counter == 100)
+                break;
         }
         System.out.println("Zbirot na prvite 100 prosti broja e: " + sum);
     }
