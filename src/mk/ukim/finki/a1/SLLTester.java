49aade5354247de988963e8981d77869fb938b33
Kostadin Krstev <krstev.kostadin@gmail.com>
2017-10-06 21:31:43 +0200

    added tests for the new methods

--- src/mk/ukim/finki/a1/SLLTester.java
+++ src/mk/ukim/finki/a1/SLLTester.java
@@ -45,7 +45,26 @@ public class SLLTester {
         System.out.println(niza.toString());
         System.out.println("The size of the Array is: " + niza.size());
 
+        niza.mirror();
+        System.out.println(niza.toString());
+        System.out.println("The size of the Array is: " + niza.size());
+
+        SLL<Integer> niza2 = new SLL<>();
+        niza2.insertFirst(new Integer(60));
+        niza2.insertBefore(new Integer(32), niza2.getFirst());
+        niza2.insertLast(new Integer(69));
+        niza2.insertAfter(new Integer(58), niza2.find(69));
+        System.out.println(niza2.toString());
+        System.out.println("The size of the Array is: " + niza2.size());
+
+        niza.merge(niza2);
+        System.out.println(niza.toString());
+        System.out.println("The size of the Array is: " + niza.size());
+
         niza.deleteList();
         System.out.println(niza.toString());
+
+        niza2.deleteList();
+        System.out.println(niza2.toString());
     }
 }
