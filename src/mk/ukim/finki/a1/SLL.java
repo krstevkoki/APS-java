7752c2420fdda81a6053d628bc8f134a4cb19bc0
Kostadin Krstev <krstev.kostadin@gmail.com>
2017-10-06 20:38:00 +0200

    rewritten the class from scratch

--- src/mk/ukim/finki/a1/SLL.java
+++ src/mk/ukim/finki/a1/SLL.java
@@ -3,134 +3,126 @@ package mk.ukim.finki.a1;
 public class SLL<E> {
     private SLLNode<E> first;
 
-    SLL() {
-        this.first = null;
-    }
-
-    public void deleteList() {
-        this.first = null;
+    public SLL() {
+        first = null;
     }
 
     public SLLNode<E> getFirst() {
         return first;
     }
 
-    public SLLNode<E> find(final E object) {
-        if (first != null) {
-            SLLNode<E> temp = first;
-            while ((temp.successor != null) && (!temp.element.equals(object))) {
-                temp = temp.successor;
-            }
-            if (temp.element.equals(object))
-                return temp;
-            else
-                System.out.println("Node is not found in the Array!!!");
-        } else
-            System.out.println("Empty Array!!!");
-        return first;
+    public void deleteList() {
+        first = null;
     }
 
     public int size() {
-        int length = 0;
+        int size = 0;
         if (first != null) {
+            size = 1;
             SLLNode<E> temp = first;
-            length = 1;
             while (temp.successor != null) {
                 temp = temp.successor;
-                ++length;
+                ++size;
             }
-            return length;
-        } else
-            return length;
+            return size;
+        }
+        return size;
     }
 
     @Override
     public String toString() {
-        String string = new String();
+        String str = new String();
         if (first != null) {
-            SLLNode<E> tmp = first;
-            string += tmp + " -> ";
-            while (tmp.successor != null) {
-                tmp = tmp.successor;
-                string += tmp + " -> ";
-            }
-        } else {
-            string = "Empty Array!!!";
-        }
-        return string;
-    }
-
-    public void insertFirst(final E object) {
-        // first = new SLLNode<>(object, first);
-        SLLNode<E> insert = new SLLNode<>(object, first);
-        first = insert;
-    }
-
-    public void insertAfter(final E object, final SLLNode<E> node) {
-        if (node != null) {
-            // node.successor = new SLLNode<>(object, node.successor);
-            SLLNode<E> insert = new SLLNode<>(object, node.successor);
-            node.successor = insert;
-        } else {
-            System.out.println("The Node is null!!!");
-        }
-    }
-
-    public void insertBefore(final E object, final SLLNode<E> node) {
-        SLLNode<E> temp = first;
-        if (first != null) {
-            if (first == node) {
-                this.insertFirst(object);
-                return;
-            }
-            while (temp.successor != node) {
+            SLLNode<E> temp = first;
+            str += temp.element + " -> ";
+            while (temp.successor != null) {
                 temp = temp.successor;
+                str += temp.element + " -> ";
             }
-            if (temp.successor == node)
-                insertAfter(object, temp);
-            else
-                System.out.println("Node is not found in the Array!!!");
-
-        } else {
-            System.out.println("Empty Array!!!");
-        }
+        } else
+            str = "Empty Array !!!";
+        return str;
     }
 
-    public void insertLast(final E object) {
+    public void insertFirst(E element) {
+        first = new SLLNode<>(element, first);
+    }
+
+    public void insertAfter(E element, SLLNode<E> node) {
+        if (node != null) {
+            node.successor = new SLLNode<>(element, node.successor);
+        } else
+            System.out.println("The node is null!");
+    }
+
+    public void insertBefore(E element, SLLNode<E> node) {
+        if (first != null) {
+            if (first.equals(node)) {
+                insertFirst(element);
+            } else {
+                SLLNode<E> temp = first;
+                while (temp.successor != null && !(temp.successor.equals(node))) {
+                    temp = temp.successor;
+                }
+                if (temp.successor.equals(node))
+                    // insertAfter(element, temp);
+                    temp.successor = new SLLNode<>(element, temp.successor);
+                else
+                    System.out.println("Node is not found in the Array!");
+            }
+        } else
+            System.out.println("Empty Array !!!");
+    }
+
+    public void insertLast(E element) {
         if (first != null) {
             SLLNode<E> temp = first;
             while (temp.successor != null) {
                 temp = temp.successor;
             }
-            // temp.successor = new SLLNode<E>(object, null);
-            SLLNode<E> ins = new SLLNode<>(object, null);
-            temp.successor = ins;
+            temp.successor = new SLLNode<>(element, null);
         } else
-            insertFirst(object);
+            insertFirst(element);
     }
 
     public void deleteFirst() {
-        if (first != null)
+        if (first != null) {
             first = first.successor;
-        else
-            System.out.println("Empty Array!!!");
+        } else {
+            System.out.println("Empty Array !!!");
+        }
     }
 
-    public void delete(final SLLNode<E> node) {
+    public void delete(SLLNode<E> node) {
         if (first != null) {
-            if (first == node) {
-                this.deleteFirst();
-                return;
+            if (first.equals(node))
+                deleteFirst();
+            else {
+                SLLNode<E> temp = first;
+                while (temp.successor.successor != null && !(temp.successor.equals(node))) {
+                    temp = temp.successor;
+                }
+                if (temp.successor.equals(node))
+                    temp.successor = node.successor;
+                else
+                    System.out.println("Node is not found in the Array!");
             }
+        } else
+            System.out.println("Empty Array !!!");
+    }
+
+    public SLLNode<E> find(E element) {
+        if (first != null) {
             SLLNode<E> temp = first;
-            while ((temp.successor != node) && (temp.successor.successor != null)) {
+            while (temp.successor != null && !(temp.element.equals(element))) {
                 temp = temp.successor;
             }
-            if (temp.successor == node)
-                temp.successor = node.successor;
+            if (temp.element.equals(element))
+                return temp;
             else
-                System.out.println("Node is not found in the Array!!!");
+                System.out.println("Node is not found in the Array!");
         } else
-            System.out.println("Empty Array!!!");
+            System.out.println("Empty Array !!!");
+        return null;
     }
 }
