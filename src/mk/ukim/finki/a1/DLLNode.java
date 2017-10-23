891f7d48f11292659c0d95dd736ee9aca0fb409b
Kostadin Krstev <krstev.kostadin@gmail.com>
2017-10-23 14:35:54 +0200

    added equals() and hashCode()

--- src/mk/ukim/finki/a1/DLLNode.java
+++ src/mk/ukim/finki/a1/DLLNode.java
@@ -12,6 +12,28 @@ public class DLLNode<E> {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        DLLNode<?> dllNode = (DLLNode<?>) o;
+        if (element != null ? !element.equals(dllNode.element) : dllNode.element != null)
+            return false;
+        if (predecessor != null ? !predecessor.equals(dllNode.predecessor) : dllNode.predecessor != null)
+            return false;
+        return succesor != null ? succesor.equals(dllNode.succesor) : dllNode.succesor == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = element != null ? element.hashCode() : 0;
+        result = 31 * result + (predecessor != null ? predecessor.hashCode() : 0);
+        result = 31 * result + (succesor != null ? succesor.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return element.toString();
     }
---
+++ src/mk/ukim/finki/lab2/DLLNode.java
@@ -0,0 +1,4 @@
+package mk.ukim.finki.lab2;
+
+public class DLLNode {
+}
